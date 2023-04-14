4ad26334a832f19691e314bd7ef4035fac328a01
2code4wsp <tlynde1043@gmail.com>
2023-04-14 11:59:43 -0700

    reinstalled package.json added seeds/ schema table

--- employee_db/schema.sql
+++ employee_db/schema.sql
@@ -4,20 +4,23 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE department(
-    id INT PRIMARY KEY, 
-    name VARCHAR(30),
-)
-CREATE TABLE role(
-    id INT PRIMARY KEY,
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    department_name VARCHAR(30) NOT NULL
+);
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
-    salary DECIMAL,
+    salary DECIMAL(10,2) NOT NULL,
     department_id INT,
-)
-CREATE TABLE employee(
-    id INT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-)
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
